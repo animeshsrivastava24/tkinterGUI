458146340d01c9e3f6e7f1e2b2be402673b081f6
Animesh_Srivastava <animeshsri.nith@gmail.com>
2017-05-21 22:18:19 +0530

    Now added File>Exit Edit>Setting About>help

--- create_menu.py
+++ create_menu.py
@@ -1,15 +1,8 @@
-"""
-Code illustration: 2.01
-Text Editor Code
-Step 1: Adding Top-level
-Step 2: Add Menubuttons
-
-@Tkinter GUI Application Development Blueprints
-"""
+# TEAM SAAS 
 from Tkinter import *
 
 
-PROGRAM_NAME = "Footprint Editor"
+PROGRAM_NAME = "3D SCANNER"
 
 root = Tk()
 root.geometry('350x350')
@@ -18,16 +11,16 @@ root.title(PROGRAM_NAME)
 menu_bar = Menu(root)  # menu begins
 
 file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Exit",command=root.quit)
 # all file menu-items will be added here next
 menu_bar.add_cascade(label='File', menu=file_menu)
 
 edit_menu = Menu(menu_bar, tearoff=0)
+edit_menu.add_command(label="Setting")
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 
-view_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label='View', menu=view_menu)
-
 about_menu = Menu(menu_bar, tearoff=0)
+about_menu.add_command(label="Help")
 menu_bar.add_cascade(label='About',  menu=about_menu)
 
 root.config(menu=menu_bar)  # menu ends
